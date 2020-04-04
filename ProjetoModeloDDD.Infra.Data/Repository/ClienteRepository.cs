a578ea2840e7334a3e56b565fff15452fa725f32
Felipe <felsborges@gmail.com>
2020-04-04 07:47:15 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoModeloDDD.Infra.Data/Repository/ClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Infra.Data.Repository
+{
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+    }
+}
